9293b7f45a94326a8474c0abe247e0c1c72f7def
sapinamin <samin@hortonworks.com>
2015-10-01 20:20:52 +0000

    Added new changes to TSolHBase.java

--- src/main/java/storm/benchmark/benchmarks/TSolHBase.java
+++ src/main/java/storm/benchmark/benchmarks/TSolHBase.java
@@ -42,8 +42,7 @@ public class TSolHBase  extends StormBenchmark {
 
   public static final String SHUFFLE = "topology.shuffle";
   public static final int DEFAULT_SHUFFLE = 1; // 0 = disabled; other = enabled
-  public static final String KAFKA_TOPIC = "kafka.topic";
-
+  
   public static final int DEFAULT_SPOUT_NUM = 4;
   public static final int DEFAULT_BOLT_NUM = 4;
   public static final int DEFAULT_BATCH_SIZE = 100;
@@ -60,41 +59,51 @@ public class TSolHBase  extends StormBenchmark {
 
     final int batchSize = BenchmarkUtils.getInt(config, BATCH_SIZE, DEFAULT_BATCH_SIZE);
     final int shuffle = BenchmarkUtils.getInt(config, SHUFFLE, DEFAULT_SHUFFLE);
-    final String topic = BenchmarkUtils.getStr(config, KAFKA_TOPIC);
-
+    
     // 1 -  Setup Trident Kafka Spout   --------
-    String zkConnString = "cn108-10.l42scl.hortonworks.com:2181";
+    String zkHost = BenchmarkUtils.getStr(config, "zookeeper.host");
+    String zkConnString = zkHost + ":2181";
+    //String zkConnString = "cn069.l42scl.hortonworks.com:2181";
+    String topicName = BenchmarkUtils.getStr(config, "kafka.topic");
+    // String topicName = "parts_4_100b";
+    String hbase_table  = BenchmarkUtils.getStr(config, "hbase_table.name");
+    String hbase_table_column = BenchmarkUtils.getStr(config, "hbase_table_column.name");
+    String namenode_host = BenchmarkUtils.getStr(config, "hdfs_namenode.host");
+    String zookeeper_znode_parent = BenchmarkUtils.getStr(config, "zookeeper.znode.parent");
+   
 
     BrokerHosts zk = new ZkHosts(zkConnString);
-    TridentKafkaConfig spoutConf = new TridentKafkaConfig(zk, topic);
+    TridentKafkaConfig spoutConf = new TridentKafkaConfig(zk, topicName);
     spoutConf.scheme = new SimpleSchemehb();
     spoutConf.ignoreZkOffsets = true;
     spoutConf.fetchSizeBytes = batchSize;
     OpaqueTridentKafkaSpout kspout = new OpaqueTridentKafkaSpout(spoutConf);
 
 
-
     // 2 -  Setup Trident HBase Bolt   --------
     SimpleTridentHBaseMapper hbaseMapper = new SimpleTridentHBaseMapper()
-            .withColumnFamily("cf1")
+            .withColumnFamily(hbase_table_column)
             .withRowKeyField(FIELD1_NAME)
             .withColumnFields(new Fields(FIELD2_NAME))
               ;
+    
+    String hbase_root_dir_value = "hdfs://" + namenode_host + ":8020/apps/hbase/data";
 
     Map<String, Object> hbConf = new HashMap<String, Object>();
-    hbConf.put("hbase.rootdir", "hdfs://cn108-10.l42scl.hortonworks.com:8020/apps/hbase/data");
+    hbConf.put("hbase.rootdir", hbase_root_dir_value);
     hbConf.put("hbase.zookeeper.property.clientPort", "2181");
-    hbConf.put("hbase.zookeeper.quorum","cn106-10.l42scl.hortonworks.com,cn107-10.l42scl.hortonworks.com,cn108-10.l42scl.hortonworks.com");
+    hbConf.put("hbase.zookeeper.quorum",zkHost);
+    hbConf.put("zookeeper.znode.parent", zookeeper_znode_parent);
     hbConf.put("hbase.zookeeper.useMulti","true");
     config.put("hbase.conf", hbConf);
 
 
-    String hbaseTable = "stormperf";
+    String hbaseTable = hbase_table;
     HBaseState.Options options = new HBaseState.Options()
             .withConfigKey("hbase.conf")
             .withDurability(Durability.SYNC_WAL)
             .withMapper(hbaseMapper)
-            .withTableName(hbaseTable);
+            .withTableName("stormperf");
 
     StateFactory factory = new HBaseStateFactory(options);
 
@@ -104,8 +113,7 @@ public class TSolHBase  extends StormBenchmark {
     TridentTopology trident = new TridentTopology();
 
 
-    Stream strm = trident.newStream("spout", kspout).groupBy()
-            .parallelismHint(spoutNum);
+    Stream strm = trident.newStream("spout", kspout).parallelismHint(spoutNum);
 
     if(shuffle!=0) {
       strm.shuffle();
